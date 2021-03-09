b1100bbc1527248ba52108650ef97dc32d5e2eb2
ardigermenji <ardi.germenji@gmail.com>
2021-03-09 16:25:42 +0100

    asd

--- assets/js/dom-references.js
+++ assets/js/dom-references.js
@@ -27,3 +27,7 @@ const sideMenuMobileBtn = document.querySelector('#sideMenuMobile a');
 const logoutDropDown = document.querySelector('.logout');
 const toasterEntryPoint = document.getElementById('toasterEntryPoint');
 const inputField = document.querySelector('.input-search');
+const countUniqueAlbumsBtn = document.getElementById('countUniqueAlbumsBtn');
+const displayUniqueAlbumCount = document.getElementById(
+  'displayUniqueAlbumCount'
+);
