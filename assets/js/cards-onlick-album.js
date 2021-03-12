f3156278303011372949e780bf8cc9a4bb331793
ardigermenji <ardi.germenji@gmail.com>
2021-03-12 14:10:10 +0100

    main branch

--- assets/js/cards-onlick-album.js
+++ assets/js/cards-onlick-album.js
@@ -1,8 +1,13 @@
 // jsEntryPoint.addEventListener('click', getCardInfo);
 
 function getCardsInfo(e) {
-  console.log('hi');
   const card = e.target.closest('.card-custom');
+  // from the card tkae the id => is in the spa
+  // card.querySelector('span')
+  // query id = sspan.id
+  // const queryID = card.getelebyid()
+  //`https://striveschool-api.herokuapp.com/api/deezer/album/${queryId}`
+  //needd to make a fetch request for the album with that id
   const title = card.querySelector('.card-title-custom').innerText;
   const img = card.querySelector('img');
   const url = img.src;
